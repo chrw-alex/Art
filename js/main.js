ca5cfd311430d0845e0a2fd9b6e76bc4662f236e
Aleksandra Chervonets <chervonets.as@gmail.com>
2022-09-15 15:40:22 +0300

    add calculator

--- js/main.js
+++ js/main.js
@@ -412,6 +412,35 @@ window.addEventListener('DOMContentLoaded', () => {
     };
   };
 
+  //Calculator
+
+  const calc = (size, material, options, promocode, result) => {
+    const sizeBlock = document.querySelector(size),
+          materialBlock = document.querySelector(material),
+          optionsBlock = document.querySelector(options),
+          promocodeBlock = document.querySelector(promocode),
+          resultBlock = document.querySelector(result);
+
+    let sum = 0;
+
+    const calcFunction = () => {
+      sum = Math.round((+sizeBlock.value) * (+materialBlock.value) + (+optionsBlock.value));
+
+      if (sizeBlock.value == '' || materialBlock.value == '') {
+        resultBlock.textContent = 'Пожалуйста выберите размер и материал картины';
+      } else if (promocodeBlock.value === 'IWANTPOPART') {
+        resultBlock.textContent = Math.round(sum * 0.7);
+      } else {
+        resultBlock.textContent = sum;
+      }
+    };
+
+    sizeBlock.addEventListener('change', calcFunction);
+    materialBlock.addEventListener('change', calcFunction);
+    optionsBlock.addEventListener('change', calcFunction);
+    promocodeBlock.addEventListener('input', calcFunction);
+  };
+
   modals();
   sliders('.feedback-slider-item', 'horizontal', '.main-prev-btn', '.main-next-btn');
   sliders('.main-slider-item', 'vertical');
@@ -419,6 +448,7 @@ window.addEventListener('DOMContentLoaded', () => {
   mask('[name="phone"]');
   checkTestInputs('[name="name"]');
   checkTestInputs('[name="message"]');
-  showMoreStyles('.button-styles', '#styles .row')
+  showMoreStyles('.button-styles', '#styles .row');
+  calc('#size', '#material', '#options', '.promocode', '.calc-price');
 });
 
