d3f749207335b0e057457486a921bd1278ab1341
Aleksandra Chervonets <chervonets.as@gmail.com>
2022-09-16 14:52:12 +0300

    add burger

--- js/main.js
+++ js/main.js
@@ -589,6 +589,29 @@ window.addEventListener('DOMContentLoaded', () => {
     // });
   };
 
+  // Burger
+
+  const burger = (menuSelector, burgerSelector) => {
+    const menuElem = document.querySelector(menuSelector),
+          burgerElem = document.querySelector(burgerSelector);
+
+    menuElem.style.display = 'none';
+
+    burgerElem.addEventListener('click', () => {
+      if (menuElem.style.display == 'none' && window.screen.availWidth < 993) {
+        menuElem.style.display = 'block';
+      } else {
+        menuElem.style.display = 'none';
+      }
+    });
+
+    window.addEventListener('resize', () => {
+      if (window.screen.availWidth > 992) {
+        menuElem.style.display = 'none';
+      }
+    });
+  };
+
   modals();
   sliders('.feedback-slider-item', 'horizontal', '.main-prev-btn', '.main-next-btn');
   sliders('.main-slider-item', 'vertical');
@@ -601,5 +624,6 @@ window.addEventListener('DOMContentLoaded', () => {
   filter();
   pictureSize('.sizes-block');
   accordion('.accordion-heading');
+  burger('.burger-menu', '.burger');
 });
 
