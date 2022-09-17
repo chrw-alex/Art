f998afe4c3ae5cedd7fbec022a6dcc8d4f5f570f
Aleksandra Chervonets <chervonets.as@gmail.com>
2022-09-17 16:12:17 +0300

    add drop

--- js/main.js
+++ js/main.js
@@ -719,6 +719,58 @@ window.addEventListener('DOMContentLoaded', () => {
     // calcScroll();
   };
 
+  //Drop
+
+  const drop = () => {
+    const fileInputs = document.querySelectorAll('[name="upload"]');
+
+    ['dragenter', 'dragleave', 'dragover', 'drop'].forEach(eventName => {
+      fileInputs.forEach(input => {
+        input.addEventListener(eventName, preventDefaults, false);
+      });
+    });
+
+    function preventDefaults(e) {
+      e.preventDefault();
+      e.stopPropagation();
+    }
+
+    function highlight(item) {
+      item.closest('.file_upload').style.border = "5px solid yellow";
+      item.closest('.file_upload').style.backgroundColor = "rgba(0,0,0, .4)";
+    }
+
+    function unhighlight(item) {
+      item.closest('.file_upload').style.border = "none";
+      item.closest('.file_upload').style.backgroundColor = "";
+    }
+
+    ['dragenter', 'dragover'].forEach(eventName => {
+      fileInputs.forEach(input => {
+        input.addEventListener(eventName, () => highlight(input), false);
+      });
+    });
+
+    ['dragleave', 'drop'].forEach(eventName => {
+      fileInputs.forEach(input => {
+        input.addEventListener(eventName, () => unhighlight(input), false);
+      });
+    });
+
+    fileInputs.forEach(input => {
+      input.addEventListener('drop', (e) => {
+        input.files = e.dataTransfer.files;
+        let dots;
+        const arr = input.files[0].name.split('.');
+
+        arr[0].length > 6 ? dots = '...' : dots = '.';
+        const name = arr[0].substring(0, 6) + dots + arr[1];
+        input.previousElementSibling.textContent = name;
+      });
+    });
+
+  };
+
   modals();
   sliders('.feedback-slider-item', 'horizontal', '.main-prev-btn', '.main-next-btn');
   sliders('.main-slider-item', 'vertical');
@@ -733,5 +785,6 @@ window.addEventListener('DOMContentLoaded', () => {
   accordion('.accordion-heading');
   burger('.burger-menu', '.burger');
   scrolling('.pageup');
+  drop();
 });
 
